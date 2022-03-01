1b5463f17d273621c59ada39994dc533a7d81b57
Miguel Lutz <miguellutz31@gmail.com>
2022-03-01 17:36:48 +0100

    Create posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.save!
+      redirect_to @post, notice: "Your post was successfully submitted. Please wait for it to be reviewed by an Admin"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
