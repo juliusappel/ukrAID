1f5071f49a29f3a40ee7ad6c8655cf90817a21f4
Julius Appel <github@juliusappel.com>
2022-03-01 23:49:33 +0100

    Added photo ability to model 'Post' and controller 'Posts'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, photos: [])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :addresses, dependent: :destroy
+  has_many_attached :photos, dependent: :destroy
 
   # Validates necessary post elements
   validates :title, :content, :vote_score, :pending, presence: true
