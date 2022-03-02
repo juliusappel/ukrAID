f9879ca9765117d8df387e7c4c01d1750c036ac0
Julius Appel <github@juliusappel.com>
2022-03-02 23:07:52 +0100

    Removed column 'target' from table 'categories'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.create(name: "Danger Zones", target: 0)
-Category.create(name: "Medical Assistance", target: 0)
-Category.create(name: "Shelters", target: 0)
-User.create!(username: 'moritz', email: 'moritz@quast.co', password: '123456', role: 1)
-Post.create(title: "WTF", content: "skjhaksjdhkajshkjajshdkjhasd", user_id: 1)
