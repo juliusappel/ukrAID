2e48aac2428bbcb1f2f06a69f772260f75b08270
Julius Appel <github@juliusappel.com>
2022-03-02 22:47:14 +0100

    Setup of models to save posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  acts_as_favoritor
 
   validates :username, :role, presence: true
   validates :username, uniqueness: true
