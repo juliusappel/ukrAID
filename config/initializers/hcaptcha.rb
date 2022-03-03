d72784627d3bace5063ac2934b86d911cf3c1b53
Julius Appel <github@juliusappel.com>
2022-03-03 18:53:15 +0100

    Added gem for hCaptcha

---
+++ config/initializers/hcaptcha.rb
@@ -0,0 +1,8 @@
+Hcaptcha.configure do |config|
+  config.site_key = 'HCAPTCHA_PUBLIC_SITE_KEY'
+  config.secret_key = 'HACAPTCHA_SECRET_KEY'
+  # optional, default value = https://hcaptcha.com/siteverify
+  config.verify_url = 'VERIFY_URL'
+  # optional, default value = https://hcaptcha.com/1/api.js
+  config.api_script_url = 'API_SCRIPT_URL'
+end
