3d4122760946751ffd1f941b6a1b78d901e6e27c
Julius Appel <github@juliusappel.com>
2022-03-03 16:32:29 +0100

    Added JS for autocomplete

---
+++ app/javascript/controllers/geocoder_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+export default class extends Controller {
+  static values = { apiKey: String }
+
+  static targets = ["address"]
+
+  connect() {
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    });
+    this.geocoder.addTo(this.element)
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
+  }
+
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"]
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
+  }
+}
