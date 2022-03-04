9d1cd403f4e49b3a5a6dea5c94cf5ce59bcb3b0a
Julius Appel <github@juliusappel.com>
2022-03-04 16:23:32 +0100

    Added association

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :addresses, dependent: :destroy
   has_many_attached :photos, dependent: :destroy
-  has_many :post_categories
+  has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
   has_rich_text :rich_content
 
