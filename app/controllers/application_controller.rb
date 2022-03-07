fa3fa788208f295c0f106e4a22d506bc57bd242c
Julius Appel <github@juliusappel.com>
2022-03-07 19:36:03 +0100

    Prepared meta tags - meta image missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[username])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
