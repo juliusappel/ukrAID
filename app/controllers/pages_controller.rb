a076b1cc7951d9bc8e68dd33f16e9dc7599c8829
Julius Appel <github@juliusappel.com>
2022-03-07 17:15:41 +0100

    Fixed merging errors from search branch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,28 +9,27 @@ class PagesController < ApplicationController
     if params[:query].present?
       @posts = Post.search_text(params[:query])
     else
-      @posts = Post.all
+      @published_posts = Post.where(pending: false)
+      case params[:location]
+      when "ukraine"
+        @categories = Category.where(target_group: 0) + Category.where(target_group: 1)
+        @posts = []
+        @published_posts.each { |p| @posts.push(p) if p.categories.any? { |c| c.target_group <= 1 } }
+      when "world"
+        @categories = Category.where(target_group: 0) + Category.where(target_group: 2)
+        @posts = []
+        @published_posts.each { |p| @posts.push(p) if p.categories.any? { |c| c.target_group != 1 } }
+      else
+        @categories = Category.all
+        @posts = @published_posts
+      end
     end
-
-    # @published_posts = Post.all
-    # # @published_posts = Post.where(pending: false)
-    # case params[:location]
-    # when "ukraine"
-    #   @categories = Category.where(target_group: 0) + Category.where(target_group: 1)
-    #   @posts = []
-    #   @published_posts.each { |p| @posts.push(p) if p.categories.any? { |c| c.target_group <= 1 } }
-    # when "world"
-    #   @categories = Category.where(target_group: 0) + Category.where(target_group: 2)
-    #   @posts = []
-    #   @published_posts.each { |p| @posts.push(p) if p.categories.any? { |c| c.target_group != 1 } }
-    # else
-    #   @categories = Category.all
-    #   @posts = @published_posts
-    # end
   end
 
   def dashboard
     @my_posts = Post.where(user_id: current_user.id)
+    @my_published_posts = Post.where(user_id: current_user.id, pending: false)
+    @my_pending_posts = Post.where(user_id: current_user.id, pending: true)
     @pending_posts = Post.where(pending: true) unless current_user.role.zero?
     @saved_posts = current_user.all_favorited
   end
