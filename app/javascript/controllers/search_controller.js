f3557b1297f19a2c5778d5e3e073f1b164d614e6
Miguel Lutz <miguellutz31@gmail.com>
2022-03-07 13:00:16 +0100

    Implemented new Icons in bottom menu and added a toggle search bar in the navbar

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "searchbar", "body", "input"]
+
+  expand(event) {
+    event.preventDefault()
+    this.searchbarTarget.classList.toggle("search-box-active")
+    this.inputTarget.focus()
+    console.log(this.searchbarTarget)
+    console.log(this.bodyTarget)
+  }
+}
